65b06f8265fc144912164f3ef7b7fe774ad9695a
zhtengw <atenzd@gmail.com>
2018-02-08 15:00:28 +0800

    Add ex1.11

--- ex1.6.cpp
+++ ex1.6.cpp
@@ -6,13 +6,16 @@ int main() {
 
 /* error code
 * 编译器提示: error: expected primary-expression before ‘<<’ token 
+* 只要去掉前两行末尾的分号;即可改正。
 *	std::cout << "The sum of " << v1;
 *		<< " and " << v2;
 *		<< " is " << v1+v2 << std::endl;
 */
 
 	// no error code
-	std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1+v2 << std::endl;
+	std::cout << "The sum of " << v1 
+		<< " and " << v2 
+		<< " is " << v1+v2 << std::endl;
 
 	return 0;
 }
